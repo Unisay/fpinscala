43ff14fbda8aa29735ee2fe46c568fae6f08bdcf
Yuriy Lazarev <unisay@gmail.com>
2015-10-28 13:57:53 +0100

    State: double, ints

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -6,6 +6,7 @@ trait RNG {
 }
 
 object RNG {
+
   // NB - this was called SimpleRNG in the book text
 
   case class Simple(seed: Long) extends RNG {
@@ -24,7 +25,7 @@ object RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
       val (a, rng2) = s(rng)
       (f(a), rng2)
@@ -35,39 +36,77 @@ object RNG {
     (if (random < 0) -(random + 1) else random, rng2)
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (random, rng2) = rng.nextInt
+    (if (random == 0) 0 else Int.MaxValue / random, rng2)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (randomInt, rng2) = rng.nextInt
+    val (randomDouble, rng3) = double(rng2)
+    ((randomInt, randomDouble), rng3)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((randomInt, randomDouble), rng2) = intDouble(rng)
+    ((randomDouble, randomInt), rng2)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (randomDouble1, rng2) = double(rng)
+    val (randomDouble2, rng3) = double(rng2)
+    val (randomDouble3, rng4) = double(rng3)
+    ((randomDouble1, randomDouble2, randomDouble3), rng4)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def rec(count: Int, generator: RNG, randomInts: List[Int]): (List[Int], RNG) = {
+      if (count == 0)
+        (randomInts, generator)
+      else {
+        val (randomInt, newGenerator) = generator.nextInt
+        rec(count - 1, newGenerator, randomInt :: randomInts)
+      }
+    }
+    rec(count, rng, List.empty)
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def intsViaFold(count: Int)(rng: RNG): (List[Int], RNG) = {
+    (1 to count).foldLeft(List.empty[Int], rng) { (state, _) =>
+      val (randomInts, generator) = state
+      val (randomInt, newGenerator) = generator.nextInt
+      (randomInt :: randomInts, newGenerator)
+    }
+  }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
 
-case class State[S,+A](run: S => (A, S)) {
+case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
     sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
     sys.error("todo")
+
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     sys.error("todo")
 }
 
 sealed trait Input
+
 case object Coin extends Input
+
 case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
+
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }
--- exercises/src/test/scala/fpinscala/state/RNGTest.scala
+++ exercises/src/test/scala/fpinscala/state/RNGTest.scala
@@ -20,4 +20,31 @@ class RNGTest extends FunSpec with MustMatchers {
 
   }
 
+  describe("double") {
+
+    it("generates random double values between [0, 1)") {
+      val (random1, rng1) = double(TestRNG(Int.MinValue, Int.MaxValue, -3))
+      val (random2, rng2) = double(rng1)
+      val (random3, _) = double(rng2)
+      (random1, random2, random3) mustBe(0.0, 1.0, -7.15827882E8)
+    }
+
+  }
+
+  describe("ints") {
+
+    it("generates a list of random integers") {
+      val predefinedValues = List(1, 2, 3)
+      val (randomIntegers, _) = ints(3)(TestRNG(predefinedValues: _*))
+      randomIntegers mustEqual predefinedValues.reverse
+    }
+
+    it("generates a list of random integers (via fold)") {
+      val predefinedValues = List(1, 2, 3)
+      val (randomIntegers, _) = intsViaFold(3)(TestRNG(predefinedValues: _*))
+      randomIntegers mustEqual predefinedValues.reverse
+    }
+
+  }
+
 }
